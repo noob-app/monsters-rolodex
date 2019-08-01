286f78661c2016c9d1f562192ed66277d5bf9ec8
Noob App <noob-app@Saileshs-MacBook-Pro.local>
2019-08-01 16:43:11 +0545

    Updates

---
+++ precache-manifest.3ede12a17d63b32c18173dad32ffb5e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d26b1f46678c9e251fdff5c9ca2040",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2b0dafa3eaf3ac544344",
+    "url": "/monsters-rolodex/static/css/main.8485ecb1.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/monsters-rolodex/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "2b0dafa3eaf3ac544344",
+    "url": "/monsters-rolodex/static/js/main.3dc30005.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
